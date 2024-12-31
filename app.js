4a5ad8b6d01de1d0391858f77bf41c207e691231
Uday0458 <166024876+Uday0458@users.noreply.github.com>
2024-12-31 10:33:47 +0530

    Create app.js
    content added

---
+++ app.js
@@ -0,0 +1,25 @@
+// Import Express
+
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+ 
+// Define a route
+
+app.get('/', (req, res) => {
+
+  res.send('Hello, Node.js!');
+
+});
+ 
+// Start the server
+
+app.listen(port, () => {
+
+  console.log(`Server running at http://localhost:${port}`);
+
+});
+
+ 
